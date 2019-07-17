23c438ac34189580db3d3fc6cf71eaf0157f3cdf
JohnAndOn <48613785+JohnAndOn@users.noreply.github.com>
2019-07-17 12:21:13 -0700

    Clarified flow between main.js and hostscript.jsx

--- js/main.js
+++ js/main.js
@@ -1,20 +1,29 @@
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
 /*global $, window, location, CSInterface, SystemPath, themeManager*/
 
+//Use for all code that updates HTML frame
+
 (function () {
     'use strict';
 
     var csInterface = new CSInterface();
-    
+    //To Call Photoshop Function - csInterface.evalScript('function + parameters')
     function init() {
                 
         initColors();
-                
-        $("#btn_analyze").click(function () {
-            csInterface.evalScript('sayHello()');
+        
+        document.getElementById('btn_analyze').addEventListener('click', function(){
+            //csInterface.evalScript('analyzeLayers()');
+            var val = csInterface.evalScript('analyzeLayers()', function(result) {
+                test(result,"div_doc");
+            })
         });
     }
-        
+    
+    function test(str, tag) {
+        document.getElementById(tag).append(str)
+    }
+
     init();
 
 }());
--- jsx/hostscript.jsx
+++ jsx/hostscript.jsx
@@ -1,6 +1,16 @@
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
 /*global $, Folder*/
 
-function sayHello(){
-    alert("hello from ExtendScript");
+//Use for all code that deals with Photoshop elements
+
+function analyzeLayers(parent){
+    if (parent == undefined) {
+        return "Reading Main Document";
+    } else {
+        return "Reading layers under " + parent;
+    }
+}
+
+function test() {
+    alert("Test is working");
 }
